6118760dde8353f73d5686bb5f6707cb15292f67
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    radxa0_tab: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_radxa0_tab.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_radxa0_tab-eng \
-    lineage_radxa0_tab-user \
-    lineage_radxa0_tab-userdebug
