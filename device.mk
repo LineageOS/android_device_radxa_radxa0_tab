3d2a305e4f823ce9a19943263eff8d28a06a556e
Bruno Martins <bgcngm@gmail.com>
2024-07-29 22:52:48 +0100

    radxa0_tab: logo: Override boot splash image
    Change-Id: I1324527b6dfcdf4ad1d91ebb3fcb11d18b790450

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,5 +9,8 @@ ifeq ($(WITH_GMS),true)
 GMS_MAKEFILE=gms_minimal.mk
 WITH_GMS_COMMS_SUITE := false
 endif
+
+## Logo
+LOGO_FILES_OVERRIDE := device/radxa/radxa0_tab/logo
 
 $(call inherit-product, device/radxa/radxa0/device.mk)
