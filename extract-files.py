9870d8ccc6027cd337a12fbfb1477cba6134cb09
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-28 02:17:40 -0500

    radxa0_tab: Shim extract script to point to main device tree
    Change-Id: I1e362296e937306f059da6e4e0c240aebc2a7921

---
+++ extract-files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import sys
+import subprocess
+
+# Path to the script you want to run
+script_path = '../radxa0/extract-files.py'
+
+# Run the script with arguments passed to this script
+subprocess.run(['python', script_path] + sys.argv[1:])
