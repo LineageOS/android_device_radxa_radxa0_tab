e3df07169a2d1f5b4d7604ba9334ecbb079608d6
Nolen Johnson <johnsonnolen@gmail.com>
2025-07-23 17:42:30 -0400

    radxa0_tab: Migrate to aml_tab kernel config fragment
    * Re-order so that main config is declared, and this is then
      appended.
    
    Change-Id: Ie84b826556f56b99411e06e59f8ee916cb67ddda

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+include device/radxa/radxa0/BoardConfig.mk
+
 ## Kernel
-TARGET_KERNEL_VARIANT_CONFIG := aml_tab_variant_defconfig
+TARGET_KERNEL_CONFIG += aml_tab.config
 
 ## Wi-Fi
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
-
-include device/radxa/radxa0/BoardConfig.mk
